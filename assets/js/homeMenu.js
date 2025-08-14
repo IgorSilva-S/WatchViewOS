02afb8cfbfc2f6ee918a5c680035463c836c641b
Igor Silva Santos <145566316+IgorSilva-S@users.noreply.github.com>
2025-08-14 17:17:30 -0300

    Added font and button

--- assets/js/homeMenu.js
+++ assets/js/homeMenu.js
@@ -4,18 +4,33 @@ const wallpaper = document.getElementById('wallpaper')
 
 document.addEventListener('contextmenu', (e) => {
     e.preventDefault()
-    if (!homeMenuOpened) {
+    let isMobile = window.matchMedia("(max-height: 500px)").matches ? true : false
+    let phoneUp = window.matchMedia("(max-width: 500px)").matches ? true : false
+    if (!homeMenuOpened && !phoneUp) {
         homeMenu.style.bottom = '0'
-        document.getElementById('watch').style.height = '40vh'
+        if (!isMobile) {
+            document.getElementById('watch').style.height = '40vh'
+        } else {
+            document.getElementById('watch').style.height = '50vh'
+        }
         document.getElementById('openHome').pause()
         document.getElementById('openHome').currentTime = 0
         document.getElementById('openHome').play()
         wallpaper.style.filter = 'blur(15px)'
         homeMenuOpened = true
-    } else {
+    } else if (!phoneUp) {
         homeMenu.removeAttribute('style')
         document.getElementById('watch').removeAttribute('style')
         wallpaper.removeAttribute('style')
         homeMenuOpened = false
     }
 })
+
+Array.from(document.getElementById('abc').children).forEach(element => {
+    element.addEventListener('click', () => {
+        homeMenu.removeAttribute('style')
+        document.getElementById('watch').removeAttribute('style')
+        wallpaper.removeAttribute('style')
+        homeMenuOpened = false
+    })
+});
