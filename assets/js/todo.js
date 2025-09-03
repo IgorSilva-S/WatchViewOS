c6e3b0930f3ade3bc8f58cc7065139bf4009d976
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2025-09-03 08:53:15 -0300

    Todo - Mode alert

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -15,11 +15,13 @@ document.getElementById('addTodo').addEventListener('click', () => {
 document.getElementById('removeTodo').addEventListener('click', () => {
     let allTodo = [...document.getElementsByClassName('todoBox')]
     if (removingTodo) {
+        document.getElementById('todoMode').innerHTML = 'Modo marcação'
         removingTodo = false
         allTodo.forEach((t) => {
             t.classList.remove('todoRemove')
         })
     } else {
+        document.getElementById('todoMode').innerHTML = 'Modo remoção'
         removingTodo = true
         allTodo.forEach((t) => {
             t.classList.add('todoRemove')
